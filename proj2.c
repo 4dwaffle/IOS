c13b8e598d7ccf134c598dccdd9fc6e1a9e0eee2
4dwaffle <vaclav.berny@outlook.cz>
2023-04-28 22:53:52 +0200

    Only few errors, best sync yet

--- proj2.c
+++ proj2.c
@@ -21,11 +21,20 @@ FILE *file;
 sem_t *queue_service1;
 sem_t *queue_service2;
 sem_t *queue_service3;
-sem_t *mutex;
+
 sem_t *write_file;
 sem_t *action;      //for iteration of A
 int *A;
 bool *closed;       //0 default, 1 after post closes
+sem_t *closed_sem;
+sem_t *mutex_q1;
+sem_t *mutex_q2;
+sem_t *mutex_q3;
+sem_t *customer_sem;
+sem_t *worker_sem;
+int *q1;
+int *q2;
+int *q3;
 
 void semaphores_init()
 {
@@ -37,6 +46,8 @@ void semaphores_init()
 
     closed = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
     *closed = 0;
+    closed_sem = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    sem_init(closed_sem, 1, 1);
 
     action = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
     sem_init(action, 1, 1);
@@ -48,11 +59,31 @@ void semaphores_init()
     queue_service3 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
     sem_init(queue_service3, 1, 0);
 
-    mutex = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-    sem_init(mutex, 1, 0);
+    mutex_q1 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    sem_init(mutex_q1, 1, 1);
+
+    mutex_q2 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    sem_init(mutex_q2, 1, 1);
+
+    mutex_q3 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    sem_init(mutex_q3, 1, 1);
+
+
+    q1 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    *q1 = 0;
+    q2 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    *q2 = 0;
+    q3 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    *q3 = 0;
+
+    customer_sem = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    sem_init(customer_sem, 1, 0);
+    
+    worker_sem = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    sem_init(worker_sem, 1, 0);
 }
 
-
+//TODO: cleanup
 void cleanup()
 {
     sem_destroy(write_file);
@@ -68,8 +99,10 @@ void cleanup()
     sem_destroy(queue_service3);
     munmap(queue_service3, sizeof(sem_t));
 
-    sem_destroy(mutex);
-    munmap(mutex, sizeof(sem_t));
+    munmap(closed, sizeof(bool));
+    sem_destroy(closed_sem);
+    munmap(closed_sem, sizeof(sem_t));
+    munmap(q1, sizeof(int));
 }
 
 void print_flush(const char * format, ...)
@@ -87,7 +120,7 @@ void print_flush(const char * format, ...)
     sem_post(write_file);
 }
 
-int parse_params(int argc, char* argv[]) //todo přepsat atoi sscanf a checkovat -1
+int parse_params(int argc, char* argv[])
 {
     if(argc != 6)
     {
@@ -139,119 +172,157 @@ int parse_params(int argc, char* argv[]) //todo přepsat atoi sscanf a checkovat
     return 0; //no error
 }
 
+void enlist_queue(int service)
+{
+    switch(service)//enlists quee X and waits for call from worker
+    {
+        case 1:
+            sem_wait(mutex_q1);
+            *q1 = *q1 + 1;
+            sem_post(mutex_q1);
+            sem_post(queue_service1);
+            break;
+        case 2:
+            sem_wait(mutex_q2);
+            *q2 = *q2 + 1;
+            sem_post(mutex_q2);
+             sem_post(queue_service2);
+            break;
+        case 3:
+            sem_wait(mutex_q3);
+            *q3 = *q3 + 1;
+            sem_post(mutex_q3);
+            sem_post(queue_service3);
+            break;
+    }
+}
+
 int customer(int idZ)
 {
     print_flush("Z %d: started", idZ);
-    srand(time(NULL));
-    usleep((rand() % TZ)*1000);
+    if(TZ != 0)
+    {
+        srand(time(NULL) * getpid());
+        int sleep_time = rand() % TZ;
+        if(sleep_time != 0)
+            usleep(sleep_time*1000);
+    }
+    sem_wait(closed_sem);   
     if(*closed)
     {
+        sem_post(closed_sem);
         print_flush("Z %d: going home", idZ);
         exit(0);
     }
     else
     {
-        srand(time(NULL));
-                
-        int service = rand() % 2;
-        service++;
-        print_flush("Z %d: entering office for a service %d", idZ, service);
-                
-        switch(service)//enlists quee X and waits for call from officer
-        {
-            case 1:
-                sem_post(queue_service1);
-                break;
-            case 2:
-                sem_post(queue_service2);
-                break;
-            case 3:
-                sem_post(queue_service3);
-                break;
-        }
+        sem_post(closed_sem);
 
-        sem_post(mutex); 
+
+        srand(time(NULL) * getpid());
+        int service = (rand() % 3) + 1;
+        print_flush("Z %d: entering office for a service %d", idZ, service);
+        
+        enlist_queue(service);
+        
+        sem_wait(worker_sem);
         print_flush("Z %d: called by office worker", idZ);
  
-        srand(time(NULL));
+        srand(time(NULL) * getpid());
         int sleep_time = rand() % 10;
-        usleep(sleep_time*1000);
+        if(sleep_time != 0)
+            usleep(sleep_time*1000);
                 
         print_flush("Z %d: going home", idZ);
         exit(0);
     }
 }
 
+int pick_queue(int idU)
+{
+    int service = 0;
+    sem_wait(mutex_q1);
+    sem_wait(mutex_q2);
+    sem_wait(mutex_q3);
+    if(*q1 == 0 && *q2 == 0 && *q3 == 0)
+    {
+        sem_wait(closed_sem);
+        if(*closed)
+        {
+            sem_post(closed_sem);            
+            print_flush("U %d: going home", idU);
+            sem_post(mutex_q1);
+            sem_post(mutex_q2);
+            sem_post(mutex_q3);
+            exit(0);    
+        }
+        else
+        {
+            sem_post(closed_sem);
+
+            print_flush("U %d: taking break", idU);
+            srand(time(NULL) * getpid());
+            int sleep_time = rand() % 10;
+            if(sleep_time != 0)
+                usleep(sleep_time* 1000);
+            print_flush("U %d: break finished", idU);
+            service = -1;
+        }
+    }
+    else if(*q1 > 0)
+    {
+        *q1 = *q1 - 1;
+        service =  1;
+        sem_wait(queue_service1);
+    }
+    else if(*q2 > 0)
+    {
+        *q2 = *q2 - 1;
+        service = 2;
+        sem_wait(queue_service2);
+    }
+    else if(*q3 > 0)
+    {
+        *q3 = *q3 - 1;
+        service = 3;
+        sem_wait(queue_service3);
+    }
+    sem_post(mutex_q1);
+    sem_post(mutex_q2);
+    sem_post(mutex_q3);
+    return service;
+}
+
 int worker(int idU)
 {
     print_flush("U %d: started", idU);
- 
-    while(*closed == 0)
+    while(1)
     {
-        srand(time(NULL));
-        usleep((rand() % 10)*1000);
-
-        /*if(q1 == 0 && q2 == 0 && q3 == 0)
-        {
-            if(*closed == 0)
-            {
-                print_flush("U %d: taking break", idU);
-                srand(time(NULL));
-                usleep((rand() % TU)*1000);
-                print_flush("U %d: break finished", idU);
-                continue;
-            }
-            else
-            {
-                break;
-            }
-            
-        }*/
-        if(*closed)
-        {
-            break;
-        }
-        
-
-        int service = rand() % 2 + 1; //get_rand_not_empty_service();
-        switch (service)
-        {
-            case 1:
-                sem_wait(queue_service1);
-                print_flush("U %d: serving a service of type %d", idU, service);
-                break;
-            case 2:
-                sem_wait(queue_service2);
-                print_flush("U %d: serving a service of type %d", idU, service);
-                break;
-            case 3:
-                sem_wait(queue_service3);
-                print_flush("U %d: serving a service of type %d", idU, service);
-                break;
-        }
-        print_flush("U %d: serving a service of type %d", idU, service);
-        sem_post(mutex);
-        srand(time(NULL));
+        srand(time(NULL) * getpid());
         int sleep_time = rand() % 10;
-        usleep(sleep_time*1000);
+        if(sleep_time != 0)
+            usleep(sleep_time * 1000);
 
-        print_flush("U %d: sservice finished", idU);
-    }
+        int service = pick_queue(idU);
+        if(service == -1)
+            continue;
+        
+        sem_post(worker_sem);
+        print_flush("U %d: serving a service of type %d", idU, service);
+
+        srand(time(NULL) * getpid());
+        sleep_time = rand() % 10;
+        if(sleep_time != 0)
+            usleep(sleep_time*1000);
     
-    print_flush("U %d: going home", idU);
-    exit(0);
+
+        print_flush("U %d: service finished", idU);
+    }
+    exit(1);
 }
 
 int main(int argc, char* argv[])
 {
-    pid_t id = fork();
-    {
-        if(id == 0)
-        {
-            usleep(F*1000);
-            *closed = 1;
-        }
-    }
     if(parse_params(argc, argv))
     {
         exit(1);
@@ -282,13 +353,18 @@ int main(int argc, char* argv[])
             worker(idU);
         }
     }
-    srand(time(NULL));
-    usleep((rand() % F/2 + F/2)*1000);
+    if(F != 0)
+        usleep(F*1000);
+    sem_wait(closed_sem);
+    *closed = 1;
+    sem_post(closed_sem);
+    srand(time(NULL) * getpid());
+    int sleep_time = rand() % F/2 + F/2;
+    if(sleep_time != 0)
+        usleep(sleep_time*1000);
 
-    while(wait(NULL) > 0){}
-
-
-    print_flush("closing\n");
+    while(wait(NULL) > 0);
+    print_flush("closing");
     cleanup();
     return 0;
 }
